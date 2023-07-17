24003c5548b630d257a1eb170ef842dda76fa5a1
Keshav335 <88079392+Keshav335@users.noreply.github.com>
2023-07-17 14:37:21 +0530

    Add files via upload

---
+++ Lecture33-CipherSchools/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send("This is some response from your first Node.js server")
+});
+
+app.get("/add",(req,res)=>{
+    let {a: firstNumber ,b: secondNumber} = req.query;
+    let sum = parseInt(firstNumber) + parseInt(secondNumber);
+    res.send({sum});
+})
+
+app.listen(8080, () => {
+    console.log(`Server is running.`);
+});
